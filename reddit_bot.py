d4153ad6cf262d90bd6bc1aef217da4dd8408659
BeLazy167 <66153057+BeLazy167@users.noreply.github.com>
2021-04-10 14:43:50 +0530

    Update reddit_bot.py

--- reddit_bot.py
+++ reddit_bot.py
@@ -3,7 +3,7 @@ import urllib.request
 def meme_installer():
     meme_list = list()
     title_list = list()
-    reddit = praw.Reddit(client_id = "OZsROIAyH5bAbA" ,client_secret = 'PhYFLRgpllL3ZPpdIQe3D5yhRWc' ,username = "yournusername",password = "yourpass" ,user_agent ="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36")
+    reddit = praw.Reddit(client_id = 'client_id' ,client_secret = 'client_secret' ,username = "yournusername",password = "yourpass" ,user_agent ="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36")
     memes = reddit.subreddit("memes")
     hotmemes = memes.top(limit = 20)
     for meme in hotmemes:
